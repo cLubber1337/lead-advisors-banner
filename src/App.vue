fcc8407141dce74d3465fb93e8a35a47bdb2e6dd
Andrey Soroko <supersoroko@gmail.com>
2024-04-26 18:25:37 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1 class="title">Hello Vue3</h1>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/600.css'
+import '@fontsource/poppins/700.css'
+import '@fontsource/roboto'
+import './styles/main.scss'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
