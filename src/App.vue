028b0bbd40e3112ddbc44f3b56ba89875643be31
Andrey Soroko <supersoroko@gmail.com>
2024-04-26 22:43:55 +0300

    feat: add hero

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import Header from '@/components/header/Header.vue'
+import Hero from '@/components/hero/Hero.vue'
 </script>
 
 <template>
   <div class="page-wrapper">
     <Header />
+    <main>
+      <Hero />
+    </main>
   </div>
 </template>
 
---
+++ src/components/hero/Hero.vue
@@ -0,0 +1,14 @@
+<script setup>
+import styles from './hero.module.scss'
+</script>
+
+<template>
+  <section :class="styles.hero">
+    <div class="container">
+      <div :class="styles.heroInner">
+        <h1 :class="styles.title">UNDER CONSTRUCTION</h1>
+        <p :class="styles.subtitle">We're making lots of improvements and will be back soon</p>
+      </div>
+    </div>
+  </section>
+</template>
